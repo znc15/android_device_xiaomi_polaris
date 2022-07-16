bc84171de9af9db4f2a27ee37566e8089220d1ab
lllgts <wuer031@126.com>
2022-07-16 08:24:48 +0000

    Revert "polaris: Make device name/product prop overrides work"
    This reverts commit ce487be8ad445551539240184cf22fd96c307825.

--- miku_polaris.mk
+++ miku_polaris.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2019,2022 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -21,12 +21,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_SYSTEM_NAME := polaris
-
 BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="polaris-user 8.0.0 OPR1.170623.032 V9.5.19.0.ODGMIFA release-keys" \
-    TARGET_PRODUCT="polaris"
+    PRODUCT_NAME="polaris"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
